7b2beea0b048b355511990127efbd951aa0def28
cinexin <migueguillamon@gmail.com>
2020-04-11 12:56:37 +0200

    Variables intro

---
+++ src/org/courses/kotlin/variables/Variables.kt
@@ -0,0 +1,24 @@
+package org.courses.kotlin.variables
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    var num1 = 8
+    var num2 = 9
+
+    var name = "Migue"
+
+    var f = 2.1
+
+    var sum = num1 + num2 + f
+    println(sum)
+
+    println(name)
+    println ("$name")
+
+    var number: Int
+    var fullName: String
+    number = 87
+    fullName = "Miguel Ángel"
+    println (name + " "+ number)
+}
