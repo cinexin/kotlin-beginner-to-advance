563beaf73abbdd3d1582f7debaee7da6a82e6282
cinexin <migueguillamon@gmail.com>
2020-04-13 11:35:36 +0200

    Basic Math operators

---
+++ src/org/courses/kotlin/operators/Math.kt
@@ -0,0 +1,10 @@
+package org.courses.kotlin.operators
+
+fun main() {
+    val multi = 12 * 4
+    val div = 9 / 78
+    var substract = 12 - 23
+    var add = 12 + 23
+
+    println ("Addition: $add / Substract: $substract / mult: $multi / div: $div")
+}
---
+++ src/org/courses/kotlin/operators/Modulus.kt
@@ -0,0 +1,8 @@
+package org.courses.kotlin.operators
+
+fun main() {
+
+    var result = 10 % 4
+
+    println ("Remainder is: $result")
+}
